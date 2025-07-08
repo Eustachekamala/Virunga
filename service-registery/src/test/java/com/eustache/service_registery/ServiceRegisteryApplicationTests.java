9a02bc43c18aaae4e0e62b402f604fda97ce4f38
eustachekamala <eustachejared25@gmail.com>
2025-07-08 14:56:55 +0300

    setup the eureka server successfully

---
+++ service-registery/src/test/java/com/eustache/service_registery/ServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eustache.service_registery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
