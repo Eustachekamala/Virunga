c48bb87ad310227da52be8def2ebcac54ea6a126
eustachekamala <eustachejared25@gmail.com>
2025-07-08 14:57:58 +0300

    setup the database successfully and connected to the service registery eureka

---
+++ virunga-service/src/main/java/com/eustache/virunga/VirungaApplication.java
@@ -0,0 +1,13 @@
+package com.eustache.virunga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirungaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirungaApplication.class, args);
+	}
+
+}
---
+++ virunga-service/src/test/java/com/eustache/virunga/VirungaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eustache.virunga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirungaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
