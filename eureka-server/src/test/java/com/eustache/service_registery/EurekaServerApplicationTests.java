e38a5962dc64408daee758262de74fea0e1a5417
eustachekamala <eustachejared25@gmail.com>
2025-07-27 17:06:43 +0300

    updated the name

---
+++ eureka-server/src/test/java/com/eustache/service_registery/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eustache.service_registery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
