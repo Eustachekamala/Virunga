4951b5e217b0866d464370b44e36fac10e036a95
eustachekamala <eustachejared25@gmail.com>
2025-08-04 21:15:46 +0300

    reorder the import in the springConfig file

--- users-service/src/main/java/com/eustache/users_service/config/SecurityConfig.java
+++ users-service/src/main/java/com/eustache/users_service/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.eustache.users_service.config;
 
 import com.eustache.users_service.filters.JWTAuthFilter;
 import com.eustache.users_service.services.CustomUserDetailsService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +11,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
