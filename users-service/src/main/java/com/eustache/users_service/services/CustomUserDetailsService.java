117c94e23afe464c62624d73553fd4db9f8405fe
eustachekamala <eustachejared25@gmail.com>
2025-08-08 20:21:57 +0300

    updated the .env file and the docker

--- users-service/src/main/java/com/eustache/users_service/services/CustomUserDetailsService.java
+++ users-service/src/main/java/com/eustache/users_service/services/CustomUserDetailsService.java
@@ -1,16 +1,16 @@
 package com.eustache.users_service.services;
 
 import com.eustache.users_service.UserDAO;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired
-    private UserDAO userDAO;
+    private final UserDAO userDAO;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userDAO.findByUsername(username)
