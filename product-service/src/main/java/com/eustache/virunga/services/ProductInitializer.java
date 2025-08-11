56b871cd4bd183a20553d6af1ec9d09a3745972d
eustachekamala <eustachejared25@gmail.com>
2025-08-11 23:41:24 +0300

    added one attribute to the inilizer class of product

--- product-service/src/main/java/com/eustache/virunga/services/ProductInitializer.java
+++ product-service/src/main/java/com/eustache/virunga/services/ProductInitializer.java
@@ -3,6 +3,7 @@ package com.eustache.virunga.services;
 import com.eustache.virunga.ProductDAO;
 import com.eustache.virunga.model.Category;
 import com.eustache.virunga.model.Product;
+import com.eustache.virunga.model.Status;
 import com.eustache.virunga.model.TypeProduct;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,16 +12,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductInitializer {
     @Bean
-    public CommandLineRunner initProduct(ProductDAO productDAO) {
+    public CommandLineRunner commandLineRunner(ProductDAO productDAO) {
         return args -> {
             Product product = new Product();
-            if (productDAO.findByName("name").isPresent()) {
+            if (productDAO.findByName("name").isEmpty()) {
                 product.setName("Screw Driver");
                 product.setDescription("description");
                 product.setCategory(Category.ELECTRICITY);
                 product.setTypeProduct(TypeProduct.NON_CONSUMABLE);
-                product.setQuantity(1);
+                product.setQuantity(30);
+                product.setStatus(Status.NON_URGENT);
                 productDAO.save(product);
+                System.out.println("Default product " + product.getName() + " has been created");
             }
         };
     }
