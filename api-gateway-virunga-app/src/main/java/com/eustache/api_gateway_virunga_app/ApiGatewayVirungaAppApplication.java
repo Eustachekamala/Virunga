22cbe06052f72c3b9a645c197d8d0b4d7ef9fcbd
eustachekamala <eustachejared25@gmail.com>
2025-08-12 23:46:09 +0300

    added an api-gateway for endpoints

---
+++ api-gateway-virunga-app/src/main/java/com/eustache/api_gateway_virunga_app/ApiGatewayVirungaAppApplication.java
@@ -0,0 +1,13 @@
+package com.eustache.api_gateway_virunga_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayVirungaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayVirungaAppApplication.class, args);
+	}
+
+}
---
+++ api-gateway-virunga-app/src/test/java/com/eustache/api_gateway_virunga_app/ApiGatewayVirungaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eustache.api_gateway_virunga_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayVirungaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
