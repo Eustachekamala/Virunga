924c90f8bf6b44d2da4e04d055b5d4bbe199afbd
eustachekamala <eustachejared25@gmail.com>
2025-12-13 22:58:00 +0300

    updated the entrypoint of the app

--- server/src/main/java/com/eustache/virunga/config/SecurityConfig.java
+++ server/src/main/java/com/eustache/virunga/config/SecurityConfig.java
@@ -10,7 +10,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://virunga-six.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
